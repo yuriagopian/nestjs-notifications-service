71540add70e24a700bcb83775e1b2ec51a49990d
yuriagopian <yuri.agopian@hotmail.com>
2022-12-13 09:19:09 -0300

    chore: add send email implementatin

--- src/mail/smtp-mail.service.ts
+++ src/mail/smtp-mail.service.ts
@@ -1,1 +1,7 @@
-export class SMTPMail {}
+import { MailService } from './mail-service';
+
+export class SMTPMail implements MailService {
+  sendEmail(): string {
+    return 'Email enviado';
+  }
+}
