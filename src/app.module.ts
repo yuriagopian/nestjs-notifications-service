160c0da208a9a0b87a153af6aed8a3fe81bd35c1
yuriagopian <yuri.agopian@hotmail.com>
2022-12-13 10:25:11 -0300

    chore: change app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { MailService } from './mail/mail.service';
-/**
-  We can use what service we need or choose, because te two services below, 
-  implements de same behavior to the class father mail.service
- */
-import { SMTPMailService } from './mail/smtp-mail.service';
-import { PostmarkMailService } from './mail/postmark.mail.service';
+
 import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [
-    PrismaService,
-    {
-      provide: MailService,
-      useClass: PostmarkMailService,
-    },
-  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
