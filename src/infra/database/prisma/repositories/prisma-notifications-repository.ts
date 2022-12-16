19c02cc8b44127598b46538a20462c94b5ac7880
yuriagopian <yuri.agopian@hotmail.com>
2022-12-15 21:24:07 -0300

    test: add a test to send notification use case

---
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -0,0 +1,18 @@
+import { Content } from 'src/application/entities/content';
+import { Notification } from 'src/application/entities/notification';
+import { NotificationsRepository } from 'src/application/repositories/notifications-repository';
+import { PrismaService } from '../prisma.service';
+export class PrismaNotificationRepository implements NotificationsRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(notification: Notification): Promise<void> {
+    await this.prismaService.notification.create({
+      data: {
+        category: notification.category,
+        content: notification.content.value,
+        recipientId: notification.recipientId,
+        readAt: notification.readAt,
+      },
+    });
+  }
+}
