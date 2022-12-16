d764f3b201e345f8ad57d21260794f55b002135d
yuriagopian <yuri.agopian@hotmail.com>
2022-12-15 21:45:38 -0300

    chore:  adjust notification controller to use notification use case

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { NotificationsRepository } from 'src/application/repositories/notifications-repository';
+import { PrismaNotificationRepository } from './prisma/repositories/prisma-notifications-repository';
 
 @Module({
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: NotificationsRepository,
+      useClass: PrismaNotificationRepository,
+    },
+  ],
 })
 export class DatabaseModule {}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notifications.controller';
 import { SendNotification } from 'src/application/use-cases/send-notification';
 @Module({
-  imports: [NotificationsController],
-  controllers: [],
+  imports: [],
+  controllers: [NotificationsController],
   providers: [SendNotification],
 })
 export class HttpModule {}
