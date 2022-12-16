b5bbce640d22eec4b66caa612d7005f496975d0e
yuriagopian <yuri.agopian@hotmail.com>
2022-12-15 21:46:09 -0300

    chore:  adjust notification controller to use notification use case

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notifications.controller';
+import { DatabaseModule } from '../database/database.module';
 import { SendNotification } from 'src/application/use-cases/send-notification';
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [NotificationsController],
   providers: [SendNotification],
 })
