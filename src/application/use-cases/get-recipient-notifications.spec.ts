44eeb23e0eb739199aecb23295b8b478fe72f239
yuriagopian <yuri.agopian@hotmail.com>
2022-12-18 19:11:07 -0300

    test:  add test to validate get recipient notifications

---
+++ src/application/use-cases/get-recipient-notifications.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryNotificationRepository } from '@test/repositories/in-memory-notifications-repository';
+import { makeNotification } from '@test/repositories/factories/notification-factory';
+import { GetRecipientNotifications } from './get-recipient-notifications';
+
+describe('Get Recipient notifications', () => {
+  it('should be able to get recipient notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationRepository();
+    const getRecipientNotifications = new GetRecipientNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-2' }),
+    );
+
+    const { notifications } = await getRecipientNotifications.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+      ]),
+    );
+  });
+});
