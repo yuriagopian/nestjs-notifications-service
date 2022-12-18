d8e4e907b0e3393b0362efcb302dfe72d8760be1
yuriagopian <yuri.agopian@hotmail.com>
2022-12-18 19:17:31 -0300

    test:  add test to validate get recipient notifications

---
+++ src/application/use-cases/read-notification.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryNotificationRepository } from '@test/repositories/in-memory-notifications-repository';
+import { Notification } from '@application/entities/notification';
+import { Content } from '@application/entities/content';
+import { NotificationNotFound } from './errors/notification-not-found';
+import { makeNotification } from '@test/repositories/factories/notification-factory';
+import { ReadNotification } from './read-notification';
+
+describe('Read Notification', () => {
+  it('should be able to read a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationRepository();
+    const readNotification = new ReadNotification(notificationsRepository);
+
+    const notification = new Notification(
+      makeNotification({ recipientId: 'example-recipient-id' }),
+    );
+
+    await notificationsRepository.create(notification);
+
+    await readNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].readAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to read a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationRepository();
+    const readNotification = new ReadNotification(notificationsRepository);
+
+    expect(() => {
+      return readNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
