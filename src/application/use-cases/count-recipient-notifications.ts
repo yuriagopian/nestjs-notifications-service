d0896568fcc859fdea344c7952dd1ad729081f1f
yuriagopian <yuri.agopian@hotmail.com>
2022-12-18 18:29:48 -0300

    feature: add count recipient notifications use case

---
+++ src/application/use-cases/count-recipient-notifications.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+interface CountRecipientNotificationsRequest {
+  recipientId: string;
+}
+
+interface CountRecipientNotificationsResponse {
+  count: number;
+}
+
+@Injectable()
+export class CountRecipientNotifications {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: CountRecipientNotificationsRequest,
+  ): Promise<CountRecipientNotificationsResponse> {
+    const { notificationId } = request;
+
+    const notification = await this.notificationsRepository.findById(
+      notificationId,
+    );
+
+    if (!notification) {
+      throw new NotificationNotFound();
+    }
+
+    notification.cancel();
+
+    await this.notificationsRepository.save(notification);
+  }
+}
